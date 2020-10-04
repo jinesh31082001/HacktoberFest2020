ea0f39f92887a27cb735391a9ef69a5fd3f5144b
jinesh31082001 <56071733+jinesh31082001@users.noreply.github.com>
2020-10-04 19:01:32 +0530

    Update Selection_sort.c

--- Data Structures and Algorithms/Algorithms/Selection_sort.c
+++ Data Structures and Algorithms/Algorithms/Selection_sort.c
@@ -41,7 +41,7 @@ int main()
 	int arr[] = {64, 25, 12, 22, 11}; 
 	int n = sizeof(arr)/sizeof(arr[0]); 
 	selectionSort(arr, n); 
-	printf("Sorted array: \n"); 
+	printf("Sorted array:\n"); 
 	printArray(arr, n); 
 	return 0; 
 } 
